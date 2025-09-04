452e5d46b792a48cea7ad046f621a3dd094efbfe
mahirsiam2004e <mahirsiam2004@gmail.com>
2025-09-04 14:09:17 +0600

    added active button with js

--- script.js
+++ script.js
@@ -4,35 +4,49 @@ const loadLessons = () => {
     .then((json) => displayLessons(json.data));
 };
 
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  lessonButtons.forEach((btn) => btn.classList.remove("active"));
+};
+
 const loadWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   //   console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayWord(data.data));
+    .then((data) => {
+      removeActive();
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("active");
+      displayWord(data.data);
+    });
 };
 
 const displayWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
-if(words.length==0){
-  wordContainer.innerHTML = `
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
           <div class="text-center bg-[#F8F8F8] col-span-full py-10 space-y-6 font-bangla">
           <img class="mx-auto" src="./assets/alert-error.png" />
             <p class="text-xl font-medium text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
             <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
         </div>`;
-  return
-}
+    return;
+  }
 
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `        <div class="bg-white rounded-xl shadow-sm text-center py-20 px-5 space-y-4 ">
-            <h2 class="font-bold text-xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি" }</h2>
+            <h2 class="font-bold text-xl">${
+              word.word ? word.word : "শব্দ পাওয়া যায়নি"
+            }</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning :"অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "not found "}</div>
+            <div class="text-2xl font-medium font-bangla">"${
+              word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"
+            } / ${word.pronunciation ? word.pronunciation : "not found "}</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]""><i class=" fa-solid
@@ -50,7 +64,7 @@ const displayLessons = (lessons) => {
   for (let lesson of lessons) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-      <button onClick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+      <button id="lesson-btn-${lesson.level_no}" onClick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
       </button>
     `;
