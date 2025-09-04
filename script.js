9c026f7fd33acc09b91081e9b3dde3dbd8841ce3
mahirsiam2004e <mahirsiam2004@gmail.com>
2025-09-04 13:38:45 +0600

    fixed null type error

--- script.js
+++ script.js
@@ -11,16 +11,28 @@ const loadWord = (id) => {
     .then((res) => res.json())
     .then((data) => displayWord(data.data));
 };
+
 const displayWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+if(words.length==0){
+  wordContainer.innerHTML = `
+          <div class="text-center bg-[#F8F8F8] col-span-full py-10 space-y-6 font-bangla">
+          <img class="mx-auto" src="./assets/alert-error.png" />
+            <p class="text-xl font-medium text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>`;
+  return
+}
+
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `        <div class="bg-white rounded-xl shadow-sm text-center py-20 px-5 space-y-4 ">
-            <h2 class="font-bold text-xl">${word.word}</h2>
+            <h2 class="font-bold text-xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি" }</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}</div>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning :"অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "not found "}</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]""><i class=" fa-solid
