67be3bd0d71c3c6bd25dc360dba90d2e68e90739
mahirsiam2004e <mahirsiam2004@gmail.com>
2025-09-05 16:25:49 +0600

    added search and load animation

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+const createElements = (arr) => {
+  const htmlElement = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElement.join(" ");
+};
+
+function pronounceWord(word) {
+  // Create a speech utterance object
+  const utterance = new SpeechSynthesisUtterance(word);
+
+  // Set the language (voices depend on browser/system)
+  utterance.lang = "en-EN"; // Better to use "en-US" or "en-GB"
+
+  // Speak the word
+  window.speechSynthesis.speak(utterance);
+}
+
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
     .then((json) => displayLessons(json.data));
 };
 
+//  animation for loading
+const LoadAnimation = (status) => {
+  if (status == true) {
+    document.getElementById("load_ani").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("word-container").classList.remove("hidden");
+    document.getElementById("load_ani").classList.add("hidden");
+  }
+};
 
 const removeActive = () => {
   const lessonButtons = document.querySelectorAll(".lesson-btn");
   lessonButtons.forEach((btn) => btn.classList.remove("active"));
 };
 
-
 const loadWord = (id) => {
+  LoadAnimation(true);
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   //   console.log(url);
   fetch(url)
@@ -24,7 +51,6 @@ const loadWord = (id) => {
     });
 };
 
-
 const displayWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
@@ -36,10 +62,9 @@ const displayWord = (words) => {
             <p class="text-xl font-medium text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
             <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
         </div>`;
+    LoadAnimation(false);
     return;
   }
-
-
 
   words.forEach((word) => {
     // console.log(word);
@@ -56,28 +81,34 @@ const displayWord = (words) => {
                 <button onclick="loadWordDetails(${
                   word.id
                 })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]""><i class=" fa-solid
+                <button onclick="pronounceWord('${
+                  word.word
+                }')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]""><i class=" fa-solid
                     fa-volume-low"></i></button>
             </div>
         </div>`;
     wordContainer.append(card);
   });
+  LoadAnimation(false);
 };
 
+const loadWordDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displyDetails(details.data);
+};
 
-  const loadWordDetails = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/word/${id}`;
-    const res = await fetch(url);
-    const details = await res.json();
-    displyDetails(details.data);
-  };
-
-  const displyDetails = (word) => {
-    console.log(word);
-    const detailsBox = document.getElementById("details-container");
+const displyDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
   detailsBox.innerHTML = `
               <div class="">
-            <h2 class="text=2xl font=bold">${word.word} ( <i class="fa-solid fa-microphone"></i> :${word.pronunciation})</h2>
+            <h2 class="text=2xl font=bold">${
+              word.word
+            } ( <i class="fa-solid fa-microphone"></i> :${
+    word.pronunciation
+  })</h2>
         </div>
         <div class="">
             <h2 class=" font=bold">Meaning</h2>
@@ -88,17 +119,13 @@ const displayWord = (words) => {
             <p>${word.sentence}</p>
         </div>
         <div class="">
-            <h2 class=" font=bold">সমার্থক শব্দ গুলো</h2>
-            <span class="btn">demo</span>
-            <span class="btn">demo</span>
-            <span class="btn">demo</span>
+        <div>${createElements(word.synonyms)}</div>
+
         </div>
 
-  `
-    document.getElementById("my_modal_5").showModal();
-  };
-
-
+  `;
+  document.getElementById("my_modal_5").showModal();
+};
 
 const displayLessons = (lessons) => {
   const levelContainer = document.getElementById("level-container");
@@ -115,4 +142,18 @@ const displayLessons = (lessons) => {
   }
 };
 
-loadLessons();
+loadLessons(); // jhamela suru
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const allWords = data.data;
+      const filterWords = allWords.filter((word) =>
+        word.word.toLowerCase().includes(searchValue)
+      );
+      displayWord(filterWords);
+    });
+});
